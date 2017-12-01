851eaef381d03f62c5c19e9de4257721549bf040
Saul <saul292.atmes@yahoo.com>
2017-11-30 22:28:55 -0500

    added information about kafka default port

--- ProvisioningScripts/KafkaScript.sh
+++ ProvisioningScripts/KafkaScript.sh
@@ -13,3 +13,4 @@ nohup bin/kafka-server-start.sh config/server.properties > ~/kafka-logs &
 # https://dzone.com/articles/installing-and-running-kafka-on-an-aws-instance
 # Note- the default Kafka server will take up 1 gig of ram. If you're running this on a t2.micro instance, you need to run the following command as well:
 # sed -i 's/-Xmx1G -Xms1G/-Xmx500m -Xms500m/' bin/kafka-server-start.sh
+#note- the default kafka port is 9092, so you will need to open it on the AWS security group. 
