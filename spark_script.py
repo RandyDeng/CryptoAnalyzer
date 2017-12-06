40887a3c102f3873b8cecb2f6887aa54cba720c4
Saul <saul292.atmes@yahoo.com>
2017-12-05 20:20:33 -0500

    added year, removed all print statements, added back in the post to DB

--- spark_script.py
+++ spark_script.py
@@ -12,32 +12,36 @@ sc = SparkContext('local',"Simple App")
 data = sc.textFile('bitcoin_history/bitstampUSD.csv')
 fromEpoch = int(sys.argv[1]) 
 
-print(fromEpoch)
+#print(fromEpoch)
 #1315922000
 HourEpoch= fromEpoch + 3600
 DayEpoch= fromEpoch + 86400
 WeekEpoch= fromEpoch + 604800
 MonthEpoch= fromEpoch + 2629743
-print("Month:"+str(MonthEpoch) + " Week"+str(WeekEpoch)+" DayEpoch"+str(DayEpoch)+" HourEpoch"+str(HourEpoch)+"\n")
+YearEpoch= fromEpoch + 31556926
+#print("Month:"+str(MonthEpoch) + " Week"+str(WeekEpoch)+" DayEpoch"+str(DayEpoch)+" HourEpoch"+str(HourEpoch)+"\n")
 #1315922024
-MonthFilteredRDD = data.filter(lambda x: (int(str(x).split(",")[0]) >= fromEpoch and int(str(x).split(",")[0]) <= MonthEpoch))
-MonthFiltered = MonthFilteredRDD.collect()
-print("Items in month:"+str(len(MonthFiltered))+"\n")
+YearFilteredRDD = data.filter(lambda x: (int(str(x).split(",")[0]) >= fromEpoch and int(str(x).split(",")[0]) <= YearEpoch))
+YearFiltered = YearFilteredRDD.collect()
+MonthFiltered = sc.parallelize(YearFiltered).filter(lambda x: (int(str(x).split(",")[0]) >= fromEpoch and int(str(x).split(",")[0]) <= MonthEpoch)).collect()
+#print("Items in month:"+str(len(MonthFiltered))+"\n")
 WeekFiltered = sc.parallelize(MonthFiltered).filter(lambda x: (int(str(x).split(",")[0]) >= fromEpoch and int(str(x).split(",")[0]) <= WeekEpoch)).collect()
-print("Items in week:"+str(len(WeekFiltered))+"\n")
+#print("Items in week:"+str(len(WeekFiltered))+"\n")
 DayFiltered = sc.parallelize(WeekFiltered).filter(lambda x: (int(str(x).split(",")[0]) >= fromEpoch and int(str(x).split(",")[0]) <= DayEpoch)).collect()
-print("Items in day:"+str(len(DayFiltered))+"\n")
+#print("Items in day:"+str(len(DayFiltered))+"\n")
 HourFiltered = sc.parallelize(DayFiltered).filter(lambda x: (int(str(x).split(",")[0]) >= fromEpoch and int(str(x).split(",")[0]) <= HourEpoch)).collect()
-print("Items in hour:"+str(len(HourFiltered))+"\n")
-FilteredSets=[MonthFiltered,WeekFiltered,DayFiltered,HourFiltered]
-count=1
+#print("Items in hour:"+str(len(HourFiltered))+"\n")
+FilteredSets=[YearFiltered,MonthFiltered,WeekFiltered,DayFiltered,HourFiltered]
+count=0
 for Set in FilteredSets:
     PriceList=[]
     for x in Set:
         PriceList.append(float(str(x).split(",")[1]))
     RunningAverage = np.convolve(PriceList, np.ones((2,))/2, mode='valid')
     N=int(round(math.sqrt(len(Set))/2)+1)
-    if(count==1):
+    if(count==0):
+        toEpoch=YearEpoch
+    elif(count==1):
         toEpoch=MonthEpoch
     elif(count==2):
         toEpoch=WeekEpoch
@@ -66,8 +70,8 @@ for Set in FilteredSets:
              "DataPoints":len(Set),
              "MomentumLine": list(moments)
             }
-    print(post)
-    #collection.insert_one(post)
+    #print(post)
+    collection.insert_one(post)
     count=count+1
 
 #submit this script with spark-submit TestScript.py
